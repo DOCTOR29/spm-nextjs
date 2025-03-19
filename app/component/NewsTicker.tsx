b5346b71f6dbfbec0dfaa4817995707aaec58363
Naveed <naveedislam545@gmail.com>
2025-03-19 16:46:48 +0530

    made static

--- app/component/NewsTicker.tsx
+++ app/component/NewsTicker.tsx
@@ -7,22 +7,9 @@ interface NewsTickerProps {
 
 export function NewsTicker({ news, speed = 1905 }: NewsTickerProps) {
   return (
-    <div className="relative -mt-16 pt-2 mb-20 overflow-hidden bg-red-900 text-white font-bold h-12">
-      <div 
-        className="whitespace-nowrap animate-scroll inline-block"
-        style={{
-          animationDuration: `${news.join(' ').length * speed}ms`,
-          animationTimingFunction: 'linear',
-          animationIterationCount: 'infinite'
-        }}
-      >
-        {news.map((item, index) => (
-          <span key={index} className="mx-8 text-lg">
-            {item}
-            <span className="mx-4 text-yellow-400">•</span>
-          </span>
-        ))}
+    <div className="relative -mt-16 pt-3 mx-auto text-center mb-20 overflow-hidden bg-red-900 text-white font-bold h-12">
+      {news}
       </div>
-    </div>
+    // </div>
   );
 }
